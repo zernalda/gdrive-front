f5eda508d3f28759e4467f38d2ba34a4ddf84679
Zernalda Septian <zernalda@point-star.com>
2020-01-21 16:36:14 +0700

    render return bulk permission

--- src/components/permissionList.js
+++ src/components/permissionList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Button from './common/Button'
+import PermissionListChild from './PermissionListChild'
 
 export class PermissionList extends Component {
     constructor() {
@@ -10,7 +12,8 @@ export class PermissionList extends Component {
     }
 
     componentDidMount() {
-        fetch('http://localhost:3001/permissionlist')
+        // fetch('http://localhost:3001/permissionlist')
+        fetch('http://localhost:3001/extdrivelist')
         .then(res => res.json())
         .then((data) => {
           console.log(data);
@@ -25,45 +28,38 @@ export class PermissionList extends Component {
       
 
     render() {
-        let objects = this.state.item;
-        let arr = []
-        console.log("render permission list: ")
-        console.log(objects);
-        Object.keys(objects).forEach(function (key) {
-            arr.push(objects[key]);
-        });
+        let allPermissionList = this.state.item;
+        console.log("ini drivelist data: ")
+        console.log(allPermissionList);
         return(
-            <div> 
-               
-            </div>
-        );
             <div>
-                <h1>All Permission List</h1>
-                <table className="table table-dark">
-                    <thead>
-                        <tr>
-                            <th scope="col">Kind</th>
-                            <th scope="col">TargetUser</th>
-                            <th scope="col">File ID</th>
-                            <th scope="col">Doc ID</th>
-                            <th scope="col">Doc Title</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {objects.map((item, i) => (
-                        <tr key={item.doc_id}>
-                            <td>{item.target_user}</td>
-                            <td>{item.fileId}</td>
-                            <td>{item.doc_id}</td>
-                            <td>{item.doc_title}</td>    
-                        </tr>
-                    ))}
-                    <ul>{arr.map(item => 
-                        <PermissionListChild key={item.label} label={item.label} value={item.value} />)}
-                    </ul>
-                    </tbody>
-                </table>
+                {Object.keys(allPermissionList).map((key) => {
+                    return(
+                        <div key={key}>
+                            <h1>{key}</h1>
+                            {allPermissionList[key].map((dataItem) => {
+                                return(
+                                    <div>
+                                        <span key={key}></span>
+                                        <ul>email : {dataItem.email}</ul>
+                                        <ul>{dataItem.message}
+                                            {/* <button>onClick</button> */}
+                                        </ul>
+                                        <ul>target user : {dataItem.target_user}</ul>
+                                        <ul>doc ID : {dataItem.doc_id}</ul>
+                                        <ul>doc title : {dataItem.doc_title}</ul>
+                                        <ul>owner : {dataItem.owner}</ul>
+                                        <ul>
+                                            <Button title="onClick"/>
+                                        </ul>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
             </div>
+            );
         }
     }
 
