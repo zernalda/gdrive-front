d0bcdcd6d21cba0cd75f7f9da92bc99f62425388
Zernalda Septian <zernalda@point-star.com>
2020-01-27 09:24:42 +0700

    dimensional array

--- src/components/PermissionListTable.js
+++ src/components/PermissionListTable.js
@@ -7,6 +7,7 @@ export class PermissionListTable extends Component {
         super();
         this.state = {
             item: [],
+            items: []
             // isListId: false
         }
     }
@@ -16,10 +17,35 @@ export class PermissionListTable extends Component {
         fetch('http://localhost:3001/permissionlist')
         .then(res => res.json())
         .then((data) => {
-          console.log(data);
+          console.log(data.values.length);
+          var test = [];
+          for(var a = 0; a<data.values.length; a++) {
+              console.log('a'+a);
+              var abc = data.values[a];
+              console.log(abc.length)
+              for(var b=0; b< abc.length; b++) {
+                var emailAddress = abc[b].email;
+                var targetu = abc[b].target_user;
+                //   test.push({emailAddress});
+                  if (emailAddress !== null && emailAddress !== undefined) {
+                    test.push({emailAddress,targetu});
+                  } else {
+                      console.log("undefined");
+                  }
+              }
+              
+            
+          }
+          
+
+
+
           this.setState({ 
             resStatus: data.status,
-            item: data.values})
+            item: data.values,
+            testsend: test,
+            // allPermissionListObject: 
+            })
         })
         .catch(
           
@@ -33,55 +59,96 @@ export class PermissionListTable extends Component {
       
 
     render() {
-        let allPermissionList = this.state.item;
+        const allPermissionList = this.state.item;
         console.log("ini drivelist table data: ")
-        console.log(allPermissionList);
+        // console.log(allPermissionList);
+        console.log(this.state.testsend);
+        let tesdata = this.state.testsend;
+
+        // const items = this.state.item.map(function(){
+        //     return <div>
+        //         <td>{items.email}</td>
+        //         <td>{items.target_user}</td>
+        //         {/* <td>a</td> */}
+        //     </div>
+        //         })
+            console.log("ini ITEMS table data: ");
+            // console.log(items);
+       if (tesdata === undefined) { 
         return (
-        <div>
-            <table className="table">
-                {Object.keys(allPermissionList).map((key) => {
-                    return (
-                    <div style={{padding:0}}>
-                        <thead className="thead-dark">
-                            <tr>
-                                <th scope="col">No.</th>
-                                <th scope="col">Owner</th>
-                                {/* <th scope="col">Document ID</th> */}
-                                <th scope="col">Target User</th>
-                                <th scope="col">Document Title</th>
-                                <th scope="col">ACTION</th>
-                            </tr>
-                        </thead>
-                        {allPermissionList[key].map((dataItem) => {
-                            if (dataItem.doc_id == null) {
-                                console.log("detected");
-                            } else{
-                                return(
-                                    <tbody id="hasil">
-                                        <tr>
-                                            <td>{key}</td>
-                                            <td>{dataItem.owner}</td>
-                                            {/* <td>{dataItem.doc_id}</td> */}
-                                            <td>{dataItem.target_user}</td>
-                                            <td>{dataItem.doc_title}</td>
-                                            <td>
-                                                <Button 
-                                                    title="Revoke"
-                                                    action = {this.confirmAction}
-                                                />
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                        )
-                                    }
-                                
-                                })} 
-                            </div>
-                            )
-                        })}     
-            </table>
-        </div>
+            <div>
+LOADING
+            </div>
         )
+       } else {
+
+                  
+        return (
+           
+
+
+             <div>
+                  { Object.keys(tesdata).map((key) => {
+                    //if (tesdata.emailAddress == null || tesdata.emailAddress == undefined) {
+                    //    console.log("DATA KOSONG");
+                    //} else {
+                    return(
+                        <tr>
+                            <td>{key}</td>
+                            <td>{tesdata[key].emailAddress}</td> 
+                        </tr>
+                    )
+                //}
+                })
+                } 
+               
+            </div>
+
+
+        // <div>
+        //     <table className="table">
+        //         { Object.keys(allPermissionList).map((key) => {
+        //             return (
+        //             <div style={{padding:0}}>
+        //                 <thead className="thead-dark">
+        //                     <tr>
+        //                         <th scope="col">No.</th>
+        //                         <th scope="col">Owner</th>
+        //                         {/* <th scope="col">Document ID</th> */}
+        //                         <th scope="col">Target User</th>
+        //                         <th scope="col">Document Title</th>
+        //                         <th scope="col">ACTION</th>
+        //                     </tr>
+        //                 </thead>
+        //                 { allPermissionList[key].map((dataItem) => {
+        //                     if (dataItem.doc_id == null) {
+        //                         console.log("detected");
+        //                     } else{
+        //                         return(
+        //                             <tbody id="hasil">
+        //                                 <tr>
+        //                                     <td>{key}</td>
+        //                                     <td>{dataItem.owner}</td>
+        //                                     {/* <td>{dataItem.doc_id}</td> */}
+        //                                     <td>{dataItem.target_user}</td>
+        //                                     <td>{dataItem.doc_title}</td>
+        //                                     <td>
+        //                                         <Button 
+        //                                             title="Revoke"
+        //                                             action = {this.confirmAction}
+        //                                         />
+        //                                     </td>
+        //                                 </tr>
+        //                             </tbody>
+        //                         )}
+        //                 })} 
+        //             </div>
+        //             )
+        //         })}     
+        //     </table>
+        // </div>
+        )
+      } 
     }
 }
 
